900b5d5f426d09a4e4d7a8b3544e7bcd11ee919a
t_sakai <takuma.sakai@d2cid.co.jp>
2023-11-21 14:59:16 +0900

    コミット

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 import { defineConfig } from "vite";
-import imageminPlugin from "@macropygia/vite-plugin-imagemin-cache";
-import sassGlobImports from "vite-plugin-sass-glob-import";
 import path from "path";
 
 const dir = {
@@ -13,17 +11,7 @@ export default defineConfig({
   root: dir.src,
   // base: "./",
   publicDir: `../${dir.publicDir}`,
-  plugins: [
-    sassGlobImports(),
-    imageminPlugin({
-      cacheDir: ".cache",
-      concurrency: 4,
-      plugins: {
-        pngquant: { quality: [0.65, 1] },
-        mozjpeg: { quality: 85 },
-      },
-    }),
-  ],
+  // plugins: [],
 
   build: {
     outDir: `../${dir.outDir}`,
